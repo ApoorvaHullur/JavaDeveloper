d2cbf038b71fd2576681df21656419e519793105
unknown <apoorvahullur.xworkz@gmail.com>
2021-12-22 12:02:50 +0530

    InheritanceEx

---
+++ inheritance/src/com/xworkz/inheritance/dto/Chrome.java
@@ -0,0 +1,29 @@
+package com.xworkz.inheritance.dto;
+
+public class Chrome extends Browser 
+{
+	
+	private float version;
+	private float size;
+	private String releasedDate;
+	
+	public Chrome() {
+	System.out.println("invoking chrome constructor");
+}
+	public String getReleasedDate() {
+		return releasedDate;
+	}
+	
+	public void SetReleasedDate(String releasedDate) {
+		this.releasedDate = releasedDate;
+		
+	}
+public float getVersion() {
+	return version;
+}
+public void SetVersion(float version) {
+	this.version = version;
+}
+
+	
+}
---
+++ table/src/com/xworkz/table/Testtable.java
@@ -0,0 +1,27 @@
+package com.xworkz.table;
+import com.xworkz.table.dto.Table;
+import com.xworkz.table.dto.Wood;
+
+public class Testtable {
+	public static void main(String []args) {
+		
+		Table table = new Table();
+			System.out.println(table.width((byte)(20)));
+			System.out.println(table.price(1000));
+			System.out.println(table.color("brown"));
+			System.out.println(table.height(40));
+			System.out.println(table.weight((short)(50)));
+			
+			Wood wood = new Wood();
+			
+			System.out.println(wood.name("villow"));
+			System.out.println(wood.types(20));
+			System.out.println(wood.color("brown"));
+			System.out.println(wood.cost(3000));
+			System.out.println(wood.helpful(true));
+			
+			
+			
+			
+		}
+}
